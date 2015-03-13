5870677e968eed0f4505d412a780d129175728c0
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-13 17:35:49 +0800

    Updates

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -2,6 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', $title)</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     {{--Jquery--}}
     {{ link_js('assets/jquery/jquery.min.js') }}
