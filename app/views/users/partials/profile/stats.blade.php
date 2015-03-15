ce69227aa6a104fbca2105f9be9efb74ba04ce6a
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-03-15 08:22:27 +0800

    Updates

--- app/views/users/partials/profile/stats.blade.php
+++ app/views/users/partials/profile/stats.blade.php
@@ -11,16 +11,16 @@
             <span class="stat-text">Ups</span></div>
         <div class="profile-stat-count"><a href="#" class="js-appreciations-count">{{ $current_user->upsCount() }}</a></div>
     </div>
-  {{--  <!-- /.profile-stat -->
+    <!-- /.profile-stat -->
     <div class="profile-stat">
         <div class="profile-stat-label profile-sidebar-label content-minor-secton-label beicons-pre beicons-pre-followers">
             <span class="stat-text">Followers</span></div>
-        <div class="profile-stat-count"><a href="#" class="js-followers-count">0</a></div>
+        <div class="profile-stat-count"><a href="#" class="js-followers-count">{{ $current_user->followers()->count() }}</a></div>
     </div>
     <!-- /.profile-stat -->
     <div class="profile-stat">
         <div class="profile-stat-label profile-sidebar-label content-minor-secton-label beicons-pre beicons-pre-following">
             <span class="stat-text">Following</span></div>
-        <div class="profile-stat-count"><a href="#" class="js-following-count">0</a></div>
-    </div>--}}
+        <div class="profile-stat-count"><a href="#" class="js-following-count">{{ $current_user->following()->count() }}</a></div>
+    </div>
     <!-- /.profile-stat --></div>
